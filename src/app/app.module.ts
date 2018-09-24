f614fb3fabf3a8f16411beb32f85398c9e34e526
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2018-09-24 11:47:24 +0300

    add GQL service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { GridModule } from '@progress/kendo-angular-grid';
 
 import { AppComponent } from './app.component';
-import { GridModule } from '@progress/kendo-angular-grid';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
+import { GraphQLModule } from './graphql.module';
+import { Apollo } from 'apollo-angular';
+import { EditService } from './edit.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     GridModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    GraphQLModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    deps: [ Apollo ],
+    provide: EditService,
+    useFactory: (apollo: Apollo) => () => new EditService(apollo)
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
